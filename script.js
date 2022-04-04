b67e5364af244423bdc7021855f4e1e47225e791
ChiKleR <40672398+ChiKleR@users.noreply.github.com>
2022-04-04 17:56:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,90 +14,147 @@
 // Automatically refresh each time a note is open: pending.
 
 
-"use strict";
-
-
-function alert_app_has_changed()
-{
-  alert("Something changed in the Google Keep Notes app since the creation of this script (it needs to be updated).");
-}
-
-
-function do_if_tag_found(tag, cb)
-{
-  alert("``do_if_tag_found`` is yet to be implemented!");
-}
-
-
-function apply_to_note(note)
-{
-  // add styles to note_title
-  // you can even apply filters depending on tags! (although manually for this version)
-
-
-  // return ``false`` to skip.
-  // return ``true`` to break.
-  return false;
-}
-
-function apply_to_note_title(note_title)
-{
-  // add styles to note_title
-
-
-  return false;
-}
-
-function apply_to_note_body(note_body)
-{
-  // example web-safe font cascade
-  note_body.style.fontFamily = "Courier New, monospace";
-  note_body.style.fontFamily = "Consolas, monospace";
-
-
-  return true;
-}
-
-
-window.addEventListener("load", function() {
+(function() {
   "use strict";
 
 
-  const elements = document.querySelectorAll("[contenteditable=\"true\"]");
-  const elements_len = elements.length;
-    let elements_idx = 2;
+  // ↓↓↓ USER ZONE ↓↓↓ //
 
-  if (elements_idx < elements_len)
+  function apply_to_note(note)
   {
-    while (elements_idx < elements_len)
+    // you can even apply filters depending on tags! (although manually for this version)
+
+
+    note.style.fontFamily = "Garamond, serif";
+
+    const prev_font_size = window.getComputedStyle(note)["font-size"];
+    note.style["font-size"] = `${(+prev_font_size.split("px")[0])+3}px`; // @TO-DO: use ``font-size-adjust`` or something similar (it's only supported in Firefox)
+
+
+    // return ``false`` to skip.
+    // return ``true`` to break.
+    return false;
+  }
+
+  function apply_to_note_title(note_title)
+  {
+    // add styles to note_title
+
+
+    return false;
+  }
+
+  function apply_to_note_body(note_body)
+  {
+    // example web-safe font cascade
+    note_body.style.fontFamily = "Courier New, monospace";
+    note_body.style.fontFamily = "Consolas, monospace";
+
+    const prev_font_size = window.getComputedStyle(note_body)["font-size"];
+    note_body.style["font-size"] = `${(+prev_font_size.split("px")[0])-2}px`; // @TO-DO: use ``font-size-adjust`` or something similar (it's only supported in Firefox)
+
+
+    return true;
+  }
+
+  // ↑↑↑ USER ZONE ↑↑↑ //
+
+
+  // ↓↓↓ USER HELPER FUNCTIONS ↓↓↓ //
+
+  function do_if_tag_found(tag, cb)
+  {
+    alert("``do_if_tag_found`` is yet to be implemented!");
+  }
+
+
+  function for_each_snippet(note, cb) // @Unimplemented
+  {
+    const snippets = [];
+
+    alert("``for_each_snippet`` is yet to be implemented!");
+
+    const snippets_len = snippets.len;
+      let snippets_idx = 0;
+
+    while (snippets_idx < snippets_len)
     {
-      if (elements_idx == 2)
-      {
-        const note_title = elements[elements_idx];
+      const block = false;
+      const name = undefined;
+      const text = `${""}`;
 
-        apply_to_note(note_title);
+      cb({ block, name, text });
 
-        apply_to_note_title(note_title);
-      }
-      else
-      if (elements_idx == 3)
-      {
-        const note_body = elements[elements_idx];
-
-        apply_to_note(note_body);
-
-        apply_to_note_body(note_body);
-      }
-      else
-      {
-        alert_app_has_changed();
-        break;
-      }
-      ++elements_idx;
+      ++snippets_idx;
     }
   }
-  else
+
+  function snippet_to_monospace(note, snippet) // @Unimplemented
   {
-    alert_app_has_changed();
+    if (snippet.block)
+    {
+      note.innerHTML.indexOf(`\`\`\`${snippet.name}\n${snippet.text}\n\`\`\``);
+    }
+    else
+    {
+      note.innerHTML.indexOf(`\`\`${snippet.text}\`\``);
+    }
   }
-}, false);
+
+  // ↑↑↑ USER HELPER FUNCTIONS ↑↑↑ //
+
+
+  // ↓↓↓ DEVELOPER ZONE ↓↓↓ //
+
+  function main()
+  {
+    const elements = document.querySelectorAll("[contenteditable=\"true\"]");
+    const elements_len = elements.length;
+        let elements_idx = 2;
+
+    if (elements_idx < elements_len)
+    {
+        while (elements_idx < elements_len)
+        {
+        if (elements_idx == 2)
+        {
+            const note_title = elements[elements_idx];
+
+            if (apply_to_note(note_title)) break;
+
+            if (apply_to_note_title(note_title)) break;
+        }
+        else
+        if (elements_idx == 3)
+        {
+            const note_body = elements[elements_idx];
+
+            if (apply_to_note(note_body)) break;
+
+            if (apply_to_note_body(note_body)) break;
+        }
+        else
+        {
+            alert_app_has_changed();
+            break;
+        }
+        ++elements_idx;
+        }
+    }
+    else
+    {
+        alert_app_has_changed();
+    }
+  }
+
+
+  function alert_app_has_changed()
+  {
+    alert("Something changed in the Google Keep Notes app since the creation of this script (it needs to be updated).");
+  }
+
+  // ↑↑↑ DEVELOPER ZONE ↑↑↑ //
+
+
+  main();
+})();
